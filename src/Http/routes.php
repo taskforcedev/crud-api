f56de078d81e1a05bc2c3400e1542523ee83f01b
Task Force Development <taskforce2eu@gmail.com>
2015-03-16 21:20:33 +0000

    Fix missing ;

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -6,7 +6,7 @@
 
 Route::group(['namespace' => 'Taskforcedev\CrudAPI\Http\Controllers', 'prefix' => 'crud'], function()
 {
-    Route::get('get/{id}', 'ApiController@show')
-    Route::get('get', 'ApiController@index')
-    Route::post('get', 'ApiController@index')
+    Route::get('get/{id}', 'ApiController@show');
+    Route::get('get', 'ApiController@index');
+    Route::post('get', 'ApiController@index');
 });
