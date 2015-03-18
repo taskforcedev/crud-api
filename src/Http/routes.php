799754ce64a194a8564be68a8c4889c924271b34
david-accenture <david.a.craig@accenture.com>
2015-03-18 10:00:57 +0000

    WiP changes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -4,16 +4,19 @@
  * Package Routes
  */
 
-Route::group(['namespace' => 'Taskforcedev\CrudAPI\Http\Controllers', 'prefix' => 'api'], function()
+Route::group(['namespace' => 'Taskforcedev\CrudAPI\Http\Controllers'], function()
 {
-    Route::get('{model}/{id}', 'ApiController@show');
-    Route::get('{model}', 'ApiController@index');
-    Route::post('{model}', 'ApiController@index');
-});
+    Route::group(['prefix' => 'api'], function()
+    {
+        Route::get('{model}/{id}', 'ApiController@show');
+        Route::get('{model}', 'ApiController@index');
+        Route::post('{model}', 'ApiController@index');
+    });
 
-Route::group(['namespace' => 'Taskforcedev\CrudAPI\Http\Controllers', 'prefix' => 'admin/crud'], function()
-{
-    Route::get('{model}/{id}', 'AdminController@show');
-    Route::get('{model}', 'AdminController@index');
-    Route::post('{model}', 'AdminController@index');
+    Route::group(['prefix' => 'admin/crud'], function()
+    {
+        Route::get('{model}/{id}', 'AdminController@show');
+        Route::get('{model}', 'AdminController@index');
+        Route::post('{model}', 'AdminController@index');
+    });
 });
