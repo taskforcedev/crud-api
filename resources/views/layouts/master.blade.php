80ee09da171e2fced074f25cda9dfc27251e786d
Taskforcedev <taskforce2eu@gmail.com>
2015-03-19 10:40:28 +0000

    Use Yeti from Bootswatch

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>{{ config('crudapi.sitename') }}</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.4/yeti/bootstrap.min.css"
     <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 </head>
 <body>
