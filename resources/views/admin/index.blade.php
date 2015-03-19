3b76e8c3a527e954faad25b7bb97a75fcbc51b7c
Taskforcedev <taskforce2eu@gmail.com>
2015-03-19 10:01:44 +0000

    Add updated and created to crud index

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,6 +8,8 @@
         @foreach($fields as $f)
             <th>{{ ucfirst($f) }}</th>
         @endforeach
+        <th>Created At</th>
+        <th>Updated At</th>
     </tr>
     <tbody>
         @if(isset($items))
@@ -16,6 +18,8 @@
                 @foreach($fields as $f)
                     <td>{{ $item->$f }}</td>
                 @endforeach
+                    <td>{{ $item->created_at }}</td>
+                    <td>{{ $item->updated_at }}</td>
                 </tr>
             @endforeach
         @endif
