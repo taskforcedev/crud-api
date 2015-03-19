5c2c65e415ecab2e7414d0ccd7ffc6f58302698a
Taskforcedev <taskforce2eu@gmail.com>
2015-03-19 10:44:48 +0000

    Fix unclosed link

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,8 +2,8 @@
 <html>
 <head>
     <title>{{ config('crudapi.sitename') }}</title>
-    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.4/yeti/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.4/yeti/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
 </head>
 <body>
 @include('crudapi::layouts._nav')
