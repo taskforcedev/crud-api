f61fe74ef22ce72e5e034e7c06ecb094145f095e
Taskforcedev <taskforce2eu@gmail.com>
2015-03-19 10:59:28 +0000

    Add bootstrap JS.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
     <title>{{ config('crudapi.sitename') }}</title>
     <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.4/yeti/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <style>
         .table>tbody>tr>td { vertical-align:middle; }
     </style>
