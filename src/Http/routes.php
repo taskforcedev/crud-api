fc6bb83f29f64ee0ada61f255509c168845d1882
Taskforcedev <taskforce2eu@gmail.com>
2015-03-19 09:06:39 +0000

    Fix CS

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -3,23 +3,18 @@
 /**
  * Package Routes
  */
-// @codingStandardsIgnoreStart
 
 Route::group(['namespace' => 'Taskforcedev\CrudAPI\Http\Controllers'], function()
 {
-    Route::group(['prefix' => 'api'], function()
-    {
+    Route::group(['prefix' => 'api'], function() {
         Route::get('{model}/{id}', 'ApiController@show');
         Route::get('{model}', 'ApiController@index');
         Route::post('{model}', 'ApiController@store');
     });
 
-    Route::group(['prefix' => 'admin/crud'], function()
-    {
+    Route::group(['prefix' => 'admin/crud'], function() {
         Route::get('{model}/{id}', 'AdminController@show');
         Route::get('{model}', 'AdminController@index');
         Route::post('{model}', 'AdminController@store');
     });
 });
-
-// @codingStandardsIgnoreEnd
