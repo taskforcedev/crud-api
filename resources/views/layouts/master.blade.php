407f4d9b48c6fadb7b41e15007d8f1b4fc14fb11
Taskforcedev <taskforce2eu@gmail.com>
2015-03-19 11:01:12 +0000

    Add jQuery.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
     <title>{{ config('crudapi.sitename') }}</title>
     <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.4/yeti/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
+    <script src="//code.jquery.com/jquery-2.1.3.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <style>
         .table>tbody>tr>td { vertical-align:middle; }
