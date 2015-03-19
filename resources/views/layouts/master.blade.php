74e3e3c2d8958846052ec49bd304ae15e8a7d0f4
Taskforcedev <taskforce2eu@gmail.com>
2015-03-19 10:25:34 +0000

    Add FA cnd link

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,11 +3,12 @@
 <head>
     <title>{{ config('crudapi.sitename') }}</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 </head>
 <body>
 @include('crudapi::layouts._nav')
-<div class="container">
-@yield('content')
-</div>
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
 </html>
