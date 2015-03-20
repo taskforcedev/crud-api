f56be29fb6da155c2911cba6f20fa4aeb7bba91f
Taskforcedev <taskforce2eu@gmail.com>
2015-03-20 13:59:40 +0000

    Building cancel logic - wip.

--- resources/views/admin/jQuery/_modalEdit.blade.php
+++ resources/views/admin/jQuery/_modalEdit.blade.php
@@ -16,15 +16,26 @@ $(function() {
             /* get the value */
             val = $(this).html();
 
-            $(this).html('<input type="text" name="'+field+'" id="'+field+'" value="'+val+'" />');
+            $(this).html('<input type="text" name="'+field+'" id="'+field+'-'+id+'" value="'+val+'" />');
         });
         
         /* Change Edit and Delete to Save and Cancel */
         saveCancelActions(id);
     }
 
+    function actionsCancel(id)
+    {
+        /* Reset the table fields */
+        var items = $('#item-' + id + ' .editable input');
+        console.log(items)
+
+        /* Change the actions */
+        defaultActions(id);
+    }
+
     function defaultActions(id)
     {
+        /* Change the actions */
         var html = '<button id="edit-'+id+'" class="btn btn-xs btn-info editButton"><i class="fa fa-pencil"></i> Edit</button> <button id="delete-'+id+'" class="btn btn-xs btn-danger deleteButton"><i class="fa fa-times"></i> Delete</button>';
         var actions = $('#actions-' + id);
         actions.html(html);
@@ -32,18 +43,24 @@ $(function() {
 
     function saveCancelActions(id)
     {
+        /* Change the actions */
         var html = '<button id="save-'+id+'" class="btn btn-xs btn-success saveButton"><i class="fa fa-floppy-o"></i> Save</button> <button id="cancel-'+id+'" class="btn btn-xs btn-warning cancelButton"><i class="fa fa-times"></i> Cancel</button>';
         var actions = $('#actions-' + id);
         actions.html(html);
-        console.log(actions);
     }
 
-    // Add button click handler
+    // Add Edit Button click handler
     $( ".editButton" ).button().on( "click", function() {
-        console.log(this);
         var id_string = this.id;
         id = id_string.substring(5);
         editItem(id);
+    });
+
+    // Add Cancel Button click handler
+    $( ".cancelButton" ).button().on( "click", function() {
+        var id_string = this.id;
+        id = id_string.substring(7);
+        actionsCancel(id);
     });
 });
 </script>
