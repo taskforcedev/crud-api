52b068e1367cbc2da21ff6efa6a1bc8039f1f55a
PrivateSniper <privatesniper@gmail.com>
2016-02-03 14:02:34 +0000

    Add route middleware (requires 5.2 laravel+ now to work).

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -5,14 +5,14 @@
  */
 
 Route::group(['namespace' => 'Taskforcedev\CrudAPI\Http\Controllers'], function () {
-    Route::group(['prefix' => 'api'], function () {
+    Route::group(['middleware' => 'web', 'prefix' => 'api'], function () {
         Route::get('{model}/{id}', 'ApiController@show');
         Route::get('{model}', 'ApiController@index');
         Route::post('{model}', 'ApiController@store');
         Route::delete('{model}/{id}', 'ApiController@destroy');
     });
 
-    Route::group(['prefix' => 'admin/crud'], function () {
+    Route::group(['middleware' => 'web', 'prefix' => 'admin/crud'], function () {
         Route::get('{model}/{id}', 'AdminController@show');
         Route::get('{model}', 'AdminController@index');
         Route::post('{model}', 'AdminController@store');
