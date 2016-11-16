813444f98802b178d7e6c16a33fde3e08ef4c777
PrivateSniper <privatesniper@gmail.com>
2016-11-16 13:01:23 +0000

    Update tests.

--- tests/Unit/ModelHelperTest.php
+++ tests/Unit/ModelHelperTest.php
@@ -13,4 +13,36 @@ class ModelHelperTest extends TestCase
 
         $this->assertFalse($crudApi->getModel());
     }
+
+    public function testGetModelUser()
+    {
+        $namespace = 'Test\\Models\\';
+
+        $options = [
+            'namespace' => $namespace,
+            'model' => 'User',
+        ];
+
+        $crudApi = new CrudApi($options);
+
+        $fqModel = $crudApi->getModel();
+
+        $this->assertEquals($namespace . 'User', $fqModel);
+    }
+
+    public function testUserGetModelCanBeInstantiated()
+    {
+        $namespace = 'Test\\Models\\';
+
+        $options = [
+            'namespace' => $namespace,
+            'model' => 'User',
+        ];
+
+        $crudApi = new CrudApi($options);
+
+        $fqModel = $crudApi->getModel();
+        $model = new $fqModel;
+        $this->assertEquals('Test\\Models\\User', get_class($model));
+    }
 }
