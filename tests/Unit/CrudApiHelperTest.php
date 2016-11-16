c28d466de5eb8abcd105a63f7cc2ce472ee0dd9f
PrivateSniper <privatesniper@gmail.com>
2016-11-16 15:04:05 +0000

    Test getRelatedField

---
+++ tests/Unit/CrudApiHelperTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Test\Unit;
+
+use Test\TestCase;
+use Taskforcedev\CrudApi\Helpers\CrudApi;
+
+class CrudApiHelperTest extends TestCase
+{
+    public function testGetRelatedFieldForUserId()
+    {
+        $crudApi = new CrudApi(['namespace' => null]);
+        $related_field = $crudApi->getRelatedField('user_id');
+        $this->assertEquals('user', $related_field);
+    }
+}
